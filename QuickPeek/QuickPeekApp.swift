2e004045e31fb177363b5af584806069ea94a331
MinGi KYUNG <MinGi.KYUNG@gmail.com>
2025-12-12 19:17:57 +0900

    Initial Commit

---
+++ QuickPeek/QuickPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPeekApp.swift
+//  QuickPeek
+//
+//  Created by 경민기 on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
